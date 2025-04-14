a8a52c228bd9c840fc06ae0af19c0dc54c232d39
Luke Snyder <las069@bucknell.edu>
2025-04-14 11:34:37 -0400

    removing unnecessary files from starter code

--- main.js
+++ main.js
@@ -26,10 +26,10 @@
 // Chrome & Edge 113+ : Enable Vulkan, Default ANGLE Vulkan, Vulkan from ANGLE, Unsafe WebGPU Support, and WebGPU Developer Features (if exsits)
 // Firefox Nightly: sudo snap install firefox --channel=latext/edge or download from https://www.mozilla.org/en-US/firefox/channel/desktop/
 
-import Renderer from '/lib/Viz/2DRenderer.js'
-import FireWorkObject from '/lib/DSViz/FireWorkObject.js'
-import ParticleSystemObject from '/lib/DSViz/ParticleSystemObject.js'
-import StandardTextObject from '/lib/DSViz/StandardTextObject.js'
+import Renderer from './lib/Viz/2DRenderer.js'
+import FireWorkObject from './lib/DSViz/FireWorkObject.js'
+import ParticleSystemObject from './lib/DSViz/ParticleSystemObject.js'
+import StandardTextObject from './lib/DSViz/StandardTextObject.js'
 
 async function init() {
   // Create a canvas tag
